902cf9961ad87f20d58ae97052dd666277687020
felipe.pucinelli <felipepucinelli@gmail.com>
2015-03-23 00:52:08 -0300

    fix web version aspect ratio

--- js/Game.js
+++ js/Game.js
@@ -225,7 +225,7 @@ Platform.Game.prototype = {
     // pixel.canvas = Phaser.Canvas.create(this.game.width * pixel.scale, this.game.height * pixel.scale);
 
     //  Create our scaled canvas. Fullscreen mode without aspect ratio
-    if (!isMobile) pixel.canvas = Phaser.Canvas.create(window.innerWidth, (window.innerWidth / (this.game.width / this.game.height)));
+    if (!isMobile) pixel.canvas = Phaser.Canvas.create(window.innerWidth, this.game.height * (window.innerHeight/this.game.height));
 
     //  Create our scaled canvas. Fullscreen mode with aspect ratio
     if (isMobile) pixel.canvas = Phaser.Canvas.create((window.innerHeight / (this.game.height / this.game.width)), window.innerHeight);
